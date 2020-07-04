a887de26f4970076af5326d83f7e0f7e7bce9fd8
Max Schmitt <max@schmitt.mx>
2020-07-04 16:43:52 +0200

    feat: enable webkit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 const express = require("express")
 const { chromium } = require("playwright-chromium")
 const { firefox } = require("playwright-firefox")
+const { webkit } = require("playwright-webkit")
 
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.get("/browser/:name", async (req, res) => {
   const browserName = req.params["name"] || "chromium"
-  if (!["chromium", "firefox"].includes(browserName)) {
+  if (!["chromium", "firefox", "webkit"].includes(browserName)) {
     return res.status(500).send(`invalid browser name (${browserName})!`)
   }
   const url = req.query.url || "https://microsoft.com"
   console.log(`Incoming request for browser '${browserName}' and URL '${url}'`)
   try {
     /** @type {import('playwright').Browser} */
-    const browser = await { chromium, firefox }[browserName].launch(browserName === "chromium" ? {
+    const browser = await { chromium, firefox, webkit }[browserName].launch(browserName === "chromium" ? {
       args: ['--no-sandbox']
     } : {})
     const page = await browser.newPage()
